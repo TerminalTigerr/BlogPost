5c2edce9716e07588ea0233ed1e31adc645a8e66
BitHero <nuhumusah874@gmail.com>
2023-05-26 22:25:30 +0000

    Defining models

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -1,11 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-class Author(models.Model):
-    """Information about the author"""
-    user = models.TextField()
-    bio = models.CharField(max_length=200)
-    profile_pic = models.ImageField(upload_to='profile_pic', blank=True)
-    date_added = models.DateTimeField(auto_now_add=True)
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
-        return self.user
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category_detail', args=[self.slug])
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='post_images', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post.title}'
