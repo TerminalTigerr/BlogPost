634fd67e2eaeea040f0bbc40eb9a239599091855
BitHero <nuhumusah874@gmail.com>
2023-05-27 14:07:44 +0000

    defining models and url patterns

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -14,7 +14,6 @@ class Category(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
     content = models.TextField()
     publication_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
